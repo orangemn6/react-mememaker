5c0fb7f81e1f99f70270fdd9761b077940cb45dd
Orangemn6 <jbensa081@gmail.com>
2021-06-15 21:24:17 -0400

    typo -_-

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -178,7 +178,7 @@ class MainPage extends React.Component {
           <div className="sidebar">
             <NavbarBrand href="/">Make-a-Meme</NavbarBrand>
             <p>
-              This is a fun 5 hour project inspired by imgur. Built with React.
+              This is a fun  project inspired by imgur. Built with React.
             </p>
             <p>
               You can add top and bottom text to a meme-template, move the text around and can save the image by downloading it.
